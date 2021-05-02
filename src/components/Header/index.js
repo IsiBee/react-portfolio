9397c74d1e4b936a6b611a67678bd1626c6185b9
Isabelle Bertram <isabellebertram@Isabelles-MacBook-Air.local>
2021-05-02 14:26:11 -0500

    added components to src directory

---
+++ src/components/Header/index.js
@@ -0,0 +1,40 @@
+import React, { useState } from 'react';
+import Nav from '../Navigation';
+import About from '../About';
+import Resume from '../Resume';
+import Contact from '../Contact';
+import Projects from '../Project';
+
+function Header() {
+    const [currentPage, handlePageChange] = useState('About');
+    const renderPage = () => {
+        switch (currentPage) {
+            case 'Resume':
+                return <Resume />
+
+            case 'Projects':
+                return <Projects />
+
+            case 'Contact':
+                return <Contact />
+
+            default: return <About />
+        }
+    };
+    return (
+        <header>
+            <h2>
+                <a data-testid="link" href="/">
+                    Isabelle Bertram
+        </a>
+            </h2>
+            <Nav currentPage={currentPage} handlePageChange={handlePageChange} />
+            <div>
+                {renderPage()}
+            </div>
+        </header>
+
+    )
+}
+
+export default Header;
---
+++ src/components/Navigation/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+
+function Nav(props) {
+    const navlinks = ['About', 'Projects', 'Contact', 'Resume'];
+    return (
+        <ul className="nav nav-tabs">
+            {navlinks.map(link => (
+                <li className="nav-item" key={link}>
+                    <a
+                        href={'#' + link.toLowerCase()}
+                        onClick={() => props.handlePageChange(link)}
+                        className={
+                            props.currentPage === link ? 'nav-link active' : 'nav-link'
+                        }
+                    >
+                        {link}
+                    </a>
+                </li>
+            ))}
+        </ul>
+    );
+}
+
+export default Nav;
