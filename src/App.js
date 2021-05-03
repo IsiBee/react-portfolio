f945ac0f47a7c1b4ec21033495639dc612c2bfa2
Isabelle Bertram <isabellebertram@Isabelles-MacBook-Air.local>
2021-05-02 19:20:41 -0500

    added footer component

--- src/App.js
+++ src/App.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import Header from './components/Header';
+import Footer from './components/Footer';
 
 function App() {
   return (
-    <div>
-      <Header />
-    </div>
+    <>
+      <div>
+        <Header />
+      </div>
+      <div>
+        <Footer />
+      </div>
+    </>
   );
 }
 
---
+++ src/components/Footer/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+
+function Footer() {
+    return (
+        <footer>
+            <a href="https://github.com/IsiBee/">
+                <i className="fab fa-github"></i>
+            </a>
+            <a href="https://www.linkedin.com/in/isabellebertram/">
+                <i class="fab fa-linkedin"></i>
+            </a>
+            <a href="https://www.facebook.com/isabelle.bertram.3">
+                <i class="fab fa-facebook"></i>
+            </a>
+        </footer>
+    )
+}
+
+export default Footer;
